c1f4da361517ca99b54ea9ce37fd91a9dcb72ec4
argenis803@gmail.com <Argenis803@gmail.com>
2023-01-31 15:03:37 -0500

    gets the name and the search term now

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let rootURL = 'https://app.ticketmaster.com/discovery/v2/events.json?'
+let url = 'https://app.ticketmaster.com/discovery/v2/events?apikey='
 
-let APIKey ='apikey=KvUvKsCiFH8SuDxoCtgOmokjYojZ6w36&locale'
-
-
-
-
+let APIKey ='KvUvKsCiFH8SuDxoCtgOmokjYojZ6w36&locale'
 
 async function getData(){
-  let res = await fetch(`${rootURL}&countryCode=US&${APIKey}&postalCode=10001&size=1`)
+  let res = await fetch(`${url}${APIKey}=*&postalCode=10001`)
   let json = await res.json()
   console.log(json._embedded.events)
 }
