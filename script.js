c205657b17219541f3bf7c19a218336c7ff375fa
argenis803@gmail.com <Argenis803@gmail.com>
2023-02-01 13:25:43 -0500

    added eventlistener to search bar

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let url = 'https://app.ticketmaster.com/discovery/v2/events?apikey='
-
 let APIKey ='KvUvKsCiFH8SuDxoCtgOmokjYojZ6w36&locale'
+
+document.getElementById('submit').addEventListener('click',(e) => {
+  e.preventDefault()
+  let searchinput = document.getElementById('searchinput').value
+  let dropdown = document.getElementById('dropdown').value
+  console.log(searchinput,dropdown);
+
+
+})
+
+
+
+
+
+
 
 async function getData(){
   let res = await fetch(`${url}${APIKey}=*&postalCode=10001`)
