f8e17ec051e4d07f0a0f1b396ecb7aa1994b20dc
argenis803@gmail.com <Argenis803@gmail.com>
2023-02-02 11:38:42 -0500

    add function to prevent users from miss typing

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
 let url = 'https://app.ticketmaster.com/discovery/v2/events?'
 let APIKey ='apikey=KvUvKsCiFH8SuDxoCtgOmokjYojZ6w36&locale'
+const search = document.getElementById('searchinput')
+const select = document.getElementById('dropdown')
 
+function preventUserFromF(){
+  select.addEventListener('change', () => { search.value = "" })
+  search.addEventListener("keypress", (event) => {
+      if (select.value === 'postalCode') {
+        if (event.key.charCodeAt() < 48 || event.key.charCodeAt() > 57)
+          event.preventDefault()    
+      }
+  
+});}
 
+function userGetInfo(){
+  document.getElementById('submit').addEventListener('click',(e) => {
+    e.preventDefault()
+    let searchinput = search.value
+    let dropdown = select.value
+    let tempURL = ''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById('submit').addEventListener('click',(e) => {
-  e.preventDefault()
-  let searchinput = document.getElementById('searchinput').value
-  let dropdown = document.getElementById('dropdown').value
-  let tempURL = ''
-
-  switch(dropdown) {
-    case 'keyword':
-       tempURL = `${url}${APIKey}=*&keyword=${searchinput}`
-      getData(tempURL)
-      break;
-    case 'postalCode':
-        tempURL = `${url}${APIKey}=*&postalCode=${searchinput}`
-      getData(tempURL)
-      break;
-     case 'city':
-        tempURL = `${url}${APIKey}=*&city=${searchinput}`
-      getData(tempURL)
-
-       break;
-
-    default:
-        tempURL = `${url}${APIKey}=*`
+    switch(dropdown) {
+      case 'keyword':
+         tempURL = `${url}${APIKey}=*&keyword=${searchinput}`
         getData(tempURL)
-  }
+        break;
+      case 'postalCode':
+          tempURL = `${url}${APIKey}=*&postalCode=${searchinput}`
+        getData(tempURL)
+        break;
+      case 'city':
+          tempURL = `${url}${APIKey}=*&city=${searchinput}`
+        getData(tempURL)
 
+        break;
 
-
-})
-
-
-
-
-
+      default:
+          tempURL = `${url}${APIKey}=*`
+          getData(tempURL)
+  }})}
 
 
 async function getData(tempURL){
@@ -60,3 +48,5 @@ async function getData(tempURL){
 }
 
 
+preventUserFromF()
+userGetInfo()
