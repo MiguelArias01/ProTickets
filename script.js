17759c39b51a22aedcb6daa023dcc4a053877a95
argenis803@gmail.com <Argenis803@gmail.com>
2023-02-03 15:01:26 -0500

    added model and other stuff

--- script.js
+++ script.js
@@ -57,20 +57,102 @@ function preventUserFromF(){
       container.innerHTML = ''
       e.innerHTML = ''
       for (let i = 0; i < data.length; i++) {
-        let cardImg = document.createElement('div')
+        let cardcontainer = document.createElement('div')
+        let cardImg = document.createElement('img')
         let cardName = document.createElement('div')
         let Image = data[i].images[0].url
+        cardcontainer.setAttribute('value', `${i}`)
+        cardcontainer.setAttribute('id',`${i}`)
         cardImg.setAttribute('class', 'cardImg')
         cardName.setAttribute('class', 'cardName')
-        cardImg.style.backgroundImage = `url('${Image}')`
-        cardName.innerText = data[i].name
-        cardImg.append(cardName)
-        container.append(cardImg)
+        cardcontainer.setAttribute('class', 'cardcontainer')
        
-        
+        cardImg.src = Image
+        cardName.innerText = data[i].name
+        container.append(cardcontainer)
+        cardcontainer.append(cardName)
+        cardcontainer.append(cardImg)  
       }
 
-     }
+      document.querySelectorAll('.cardcontainer')
+      .forEach(el => {
+        el.addEventListener('mouseover', e => {
+          e.target.classList.add('active')
+          e.target
+        }
+        )
+          
+        el.addEventListener('mouseout', e => {
+          e.target.classList.remove('active')
+                }
+               
+              )
+        el.addEventListener('click', e => {
+          let eventNumber = e.target.getAttribute("value")
+          console.log(data[eventNumber])
+          // Get the modal
+            var modal = document.getElementById("myModal");
+
+          // Get the button that opens the modal
+             var cardBtnValue = document.getElementById(`${eventNumber}`);
+
+// Get the <span> element that closes the modal
+             var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+            cardBtnValue.onclick = function() {
+              modal.style.display = "block";
+            }
+
+// When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+              modal.style.display = "none";
+            }
+
+// When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+               }
+             }
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        })
+         
+        }
+      )
+    }
   
 
 
