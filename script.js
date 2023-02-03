1993f42ed99482ae1b897e5cb68db6056d38d3b2
argenis803@gmail.com <Argenis803@gmail.com>
2023-02-03 16:36:43 -0500

    added a few error fixes

--- script.js
+++ script.js
@@ -90,26 +90,25 @@ function preventUserFromF(){
         el.addEventListener('click', e => {
           let eventNumber = e.target.getAttribute("value")
       
-          // Get the modal
-            var modal = document.getElementById("myModal");
 
-          // Get the button that opens the modal
-             var cardBtnValue = document.getElementById(`${eventNumber}`);
+             let modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-             var span = document.getElementsByClassName("close")[0];
-             // When the user clicks on the button, open the modal
+
+             let cardBtnValue = document.getElementById(`${eventNumber}`);
+
+
+             let span = document.getElementsByClassName("close")[0];
+
              cardBtnValue.onclick = function() {
               modal.style.display = "block";
               const modelContent = document.querySelector('.modal-content')
-              modelContent.innerHTML = ''
-              console.log(data[eventNumber].dates.start.localDate)
+              modelContent.removeChild
               let div = document.createElement('div')
               let eventDate = `Date ${data[eventNumber].dates.start.localDate}`
               div.append(eventDate)
               let name = data[eventNumber].name
               let eventLocDiv = document.createElement('div')
-              eventLocDiv.innerText = `${data[eventNumber]._embedded.venues[0].name}`
+              eventLocDiv.innerText = `Venu: ${data[eventNumber]._embedded.venues[0].name}`
 
              
                
@@ -127,10 +126,7 @@ function preventUserFromF(){
               
             }
 
-// When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-              modal.style.display = "none";
-            }
+
 
 // When the user clicks anywhere outside of the modal, close it
             window.onclick = function(event) {
